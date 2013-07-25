55d2d86fda4bff2a5a222b5a2045f49e03c2a048
frankiesardo <fran.sardo@gmail.com>
2013-07-25 17:47:09 +0100

    Add sample lib files that depend on the Android API

---
+++ gradle/lib/src/main/java/com.novoda.stackoverflow.util/Fragments.java
@@ -0,0 +1,21 @@
+package com.novoda.stackoverflow.util;
+
+public final class Fragments {
+
+    private Fragments() {
+        throw new IllegalAccessError("This class should never be instantiated");
+    }
+
+    /**
+     * Simpler version of {@link android.support.v4.app.FragmentManager#findFragmentById(int)}} which infers the target type.
+     */
+    @SuppressWarnings({"unchecked", "UnusedDeclaration"})
+    public static <T> T findFragmentById(FragmentManager fragmentManager, int fragmentId) {
+        return (T) fragmentManager.findFragmentById(fragmentId);
+    }
+
+    public static <T> T findFragmentByTag(FragmentManager fragmentManager, String fragmentTag) {
+        return (T) fragmentManager.findFragmentByTag(fragmentTag);
+    }
+
+}
---
+++ gradle/lib/src/main/java/com.novoda.stackoverflow.util/Views.java
@@ -0,0 +1,26 @@
+package com.novoda.stackoverflow.util;
+
+import javax.swing.text.View;
+
+public final class Views {
+
+    private Views() {
+    }
+
+    /**
+     * Simpler version of {@link View#findViewById(int)} which infers the target type.
+     */
+    @SuppressWarnings({"unchecked", "UnusedDeclaration"})
+    public static <T extends View> T findById(View view, int id) {
+        return (T) view.findViewById(id);
+    }
+
+    /**
+     * Simpler version of {@link Activity#findViewById(int)} which infers the target type.
+     */
+    @SuppressWarnings({"unchecked", "UnusedDeclaration"})
+    public static <T extends View> T findById(Activity activity, int id) {
+        return (T) activity.findViewById(id);
+    }
+
+}
