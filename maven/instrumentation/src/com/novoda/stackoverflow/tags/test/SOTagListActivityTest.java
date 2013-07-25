77b83e15b2e886c3301fd621130e670788bd7608
Peter <peter.budo@gmail.com>
2013-07-25 12:08:16 +0100

    Added simple instrumentation test for UI component presence

--- maven/instrumentation/src/com/novoda/stackoverflow/tags/test/SOTagListActivityTest.java
+++ maven/instrumentation/src/com/novoda/stackoverflow/tags/test/SOTagListActivityTest.java
@@ -1,18 +1,33 @@
 package com.novoda.stackoverflow.tags.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
 
+import com.novoda.stackoverflow.R;
 import com.novoda.stackoverflow.tags.SOTagListActivity;
 
+import static android.test.ViewAsserts.assertOnScreen;
+
 public class SOTagListActivityTest extends ActivityInstrumentationTestCase2<SOTagListActivity> {
+
+    private SOTagListActivity activity;
 
     public SOTagListActivityTest() {
         super(SOTagListActivity.class);
     }
 
+    @Override
+    public void setUp() throws Exception {
+        activity = getActivity();
+    }
+
     public void testActivity() {
-        SOTagListActivity activity = getActivity();
         assertNotNull(activity);
+    }
+
+    public void testUIComponentsPresent() {
+        View view = activity.getWindow().getDecorView();
+        assertOnScreen(view, view.findViewById(R.id.simple_list));
     }
 }
 
